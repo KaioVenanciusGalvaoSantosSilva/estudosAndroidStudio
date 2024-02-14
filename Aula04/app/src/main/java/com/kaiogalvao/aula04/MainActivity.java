414a5df03c69fead5747232b60b3e88e85f8f4f1
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-02-14 14:51:12 -0300

    Aula 04

---
+++ Aula04/app/src/main/java/com/kaiogalvao/aula04/MainActivity.java
@@ -0,0 +1,35 @@
+package com.kaiogalvao.aula04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView txtView_Titulo = findViewById(R.id.textView_Titulo);
+        Button btn_Mudar = findViewById(R.id.btn_mudar);
+        Button btn_Reset = findViewById(R.id.btn_resetar);
+
+        btn_Mudar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtView_Titulo.setText("Meu Titulo");
+                btn_Mudar.setText("Mudei");
+            }
+        });
+
+        btn_Reset.setOnClickListener(view -> {
+            txtView_Titulo.setText("Titulo");
+            btn_Mudar.setText("Mudar");
+        });
+    }
+}
