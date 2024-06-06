a304baea3dce4a35e6fa2790bd48f12d43400356
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-05 22:43:59 -0300

    Estrutura de decisão if, if e else

--- Aula18/app/src/main/java/com/example/myapplication/Caso2.java
+++ Aula18/app/src/main/java/com/example/myapplication/Caso2.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,30 @@ public class Caso2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void verificar_btn(View view){
+
+        EditText numero = findViewById(R.id.txt_numero);
+        double valor = Double.parseDouble(numero.getText().toString());
+        //Estrutura de decisão simples apenas com 1 if
+        if (valor >= 6){
+            Toast.makeText(this, "O aluno passou com nota: "+valor, Toast.LENGTH_SHORT).show();
+        }else{
+
+            Toast.makeText(this, "O aluno reprovou com nota: "+valor, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    public void caso1(View view){
+
+        finish();
+    }
+    public void caso3(View view){
+
+        Intent intent = new Intent(this, Caso3.class);
+        startActivity(intent);
     }
 }
